8c32111fabe484c4c5028fe2f9ff10112d5b0d0a
keer2345 <keer2345@gmail.com>
2021-05-15 16:54:26 +0800

    spring security basic init

---
+++ securityBasics/src/main/java/com/keer/spring/securityBasics/SecurityBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.keer.spring.securityBasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBasicsApplication.class, args);
+	}
+
+}
---
+++ securityBasics/src/test/java/com/keer/spring/securityBasics/SecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keer.spring.securityBasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
