46ee9766b00cbe1c21c4a63465788457e22fa52d
keer2345 <keer2345@gmail.com>
2021-05-18 10:08:12 +0800

    bezkoder-jwt init

---
+++ bezkoder/bezkoder-jwt/src/test/java/com/spring/bezkoder/jwt/bezkoderjwt/BezkoderJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.bezkoder.jwt.bezkoderjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BezkoderJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
