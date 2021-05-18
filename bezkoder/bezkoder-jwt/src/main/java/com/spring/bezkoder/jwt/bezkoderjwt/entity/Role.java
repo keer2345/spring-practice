3c77d9e057fac893359269fd6a937e5a3b4c9242
keer2345 <keer2345@gmail.com>
2021-05-18 10:47:56 +0800

    role and user

---
+++ bezkoder/bezkoder-jwt/src/main/java/com/spring/bezkoder/jwt/bezkoderjwt/entity/Role.java
@@ -0,0 +1,39 @@
+package com.spring.bezkoder.jwt.bezkoderjwt.entity;
+
+import com.spring.bezkoder.jwt.bezkoderjwt.entity.audit.ERole;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  private ERole name;
+
+  public Role() {}
+
+  public Role(ERole name) {
+    this.name = name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public ERole getName() {
+    return name;
+  }
+
+  public void setName(ERole name) {
+    this.name = name;
+  }
+}
---
+++ bezkoder/bezkoder-jwt/src/main/java/com/spring/bezkoder/jwt/bezkoderjwt/entity/audit/ERole.java
@@ -0,0 +1,7 @@
+package com.spring.bezkoder.jwt.bezkoderjwt.entity.audit;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
