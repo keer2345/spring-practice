ba331d5b4cb88f6fa8ab5c2cf5d75db2b2ac6677
keer2345 <keer2345@gmail.com>
2021-05-19 11:52:16 +0800

    WebSecurityConfig.java

--- bezkoder/bezkoder-jwt/src/main/java/com/spring/bezkoder/jwt/bezkoderjwt/security/WebSecurityConfig.java
+++ bezkoder/bezkoder-jwt/src/main/java/com/spring/bezkoder/jwt/bezkoderjwt/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package com.spring.bezkoder.jwt.bezkoderjwt.security;
 
 import com.spring.bezkoder.jwt.bezkoderjwt.security.jwt.AuthEntryPointJwt;
+import com.spring.bezkoder.jwt.bezkoderjwt.security.jwt.AuthTokenFilter;
 import com.spring.bezkoder.jwt.bezkoderjwt.security.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,10 +26,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired private UserDetailsServiceImpl userDetailsService;
   @Autowired private AuthEntryPointJwt unauthorizedHandler;
 
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
 
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+  }
+
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    super.configure(http);
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .exceptionHandling()
+        .authenticationEntryPoint(unauthorizedHandler)
+        .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers("/api/auth/**")
+        .permitAll()
+        .antMatchers("/api/test/**")
+        .permitAll()
+        .anyRequest()
+        .authenticated();
+
+    http.addFilterBefore(
+        authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 }
