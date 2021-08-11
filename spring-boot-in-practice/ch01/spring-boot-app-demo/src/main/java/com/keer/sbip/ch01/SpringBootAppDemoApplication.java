93739e218933d594dcb61f1f313f2628ac11d392
keer2345 <keer2345@gmail.com>
2021-08-11 12:26:41 +0800

    spring-boot-in-practice/ch01/spring-boot-app-demo

---
+++ spring-boot-in-practice/ch01/spring-boot-app-demo/src/main/java/com/keer/sbip/ch01/SpringBootAppDemoApplication.java
@@ -0,0 +1,11 @@
+package com.keer.sbip.ch01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAppDemoApplication.class, args);
+    }
+}
